ad8426f91732aafec08afd9e0762487fe61b47da
Doms61 <dominik.lukac.96@gmail.com>
2016-11-16 13:02:53 +0100

    header

--- DBReader.c
+++ DBReader.c
@@ -6,22 +6,29 @@
  */
 
 #include <stdio.h>
+#include "dbreader.h"
 
 int main()
 {
 	FILE *fp;
 	char buff[255];
-	char c;
-	char number[6];
-	char name[20];
-
+	dbreader_t *s = newDBReader();
+	dbreader_t *t = newDBReader();
+	dbreader_t *c = newDBReader();
+	dbreader_t *a = newDBReader();
+	dbreader_t *e = newDBReader();
 
 	fp = fopen("C:/Users/Dominik/Cworkspace/tmp/DB.txt", "r");
 
 	while(!(fgets(buff, 255, (FILE*) fp) == NULL))
 	{
+
+
+
+		/*
 		sscanf(buff, "%c %s %s", &c, number, name);
 		printf("%c %s %s\n", c, number, name);
+		*/
 	}
 
 	fclose(fp);
---
+++ DBReader.h
@@ -0,0 +1,25 @@
+/*
+ * DBReader.h
+ *
+ *  Created on: 16. 11. 2016
+ *      Author: Dominik
+ */
+
+#ifndef DBREADER_H_
+#define DBREADER_H_
+
+#include "node.h"
+
+typedef struct dbreader
+{
+	node_t *sList = newNode();
+	node_t *tList = newNode();
+	node_t *cList = newNode();
+	node_t *aList = newNode();
+	node_t *eList = newNode();
+} dbreader_t;
+
+
+#endif /* DBREADER_H_ */
+
+
