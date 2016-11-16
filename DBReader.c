7509bc93ba069781aac7583ce0b6ba236e8ae5b0
Vladimir Rocin <vlada.rocin@gmail.com>
2016-11-16 14:43:22 +0100

    merge

--- DBReader.c
+++ DBReader.c
@@ -7,6 +7,64 @@
 
 #include "dbreader.h"
 
+int newDBReader(FILE *fp)
+{
+	char buff[255];
+	char c;
+	char numberP[6];
+	char numberC[6];
+	char name[100];
+	char semNumber;
+	dbreader_t *s = newDBReader();
+
+	//fp = fopen("C:/Users/Dominik/Cworkspace/tmp/DB.txt", "r");
+
+	while(!(fgets(buff, 255, (FILE*) fp) == NULL))
+	{
+		sscanf(buff, "%c ", &c);
+		if(c=='S')
+		{
+			sscanf(buff, "%c %s %s", &c, numberP, name);
+			person_t *student = newStudent(name, numberP);
+			s->sList = student;
+		}
+		else if (c == 'T')
+		{
+			sscanf(buff, "%c %s %s", &c, numberP, name);
+			person_t *teacher = newTeacher(name, numberP);
+			s->tList = teacher;
+		}
+		else if(c=='C')
+		{
+			sscanf(buff, "%c %s %s %c", &c, numberC, name, semNumber);
+			course_t *course = newCourse(name, numberC, semNumber);
+			s->cList = course;
+		}
+		else if(c=='A')
+		{
+			sscanf(buff, "%c %s %s", &c, numberP, numberC);
+			enrol_t *teacherE = newEnrol(numberP, numberC);
+			s->aList = teacherE;
+		}
+		else if(c=='E')
+		{
+			sscanf(buff, "%c %s %s", &c, numberP, numberC);
+			enrol_t *studentE = newEnrol(numberP, numberC);
+			s->eList = studentE;
+		}
+		/*
+		sscanf(buff, "%c %s %s", &c, number, name);
+		printf("%c %s %s\n", c, number, name);
+		*/
+	}
+
+	fclose(fp);
+
+	return 0;
+
+}
+
+
 node_t* findSbyName(char name[], dbreader_t* db ){
 	node_t*cur = db->sList;
 	while(cur != 0)
